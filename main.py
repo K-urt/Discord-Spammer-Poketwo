cc652cdf823fc62e77c307276a19646d7ca6d4e6
K-urt <131893438+K-urt@users.noreply.github.com>
2024-10-11 12:03:30 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from webserver import keep_alive
 import requests
 
-channelID = PUT THE CHANNEL ID
+channelID = 1100680494766620758
 headers = {
     "authorization":
-    "YOUR TOKEN HERE"
+    "MTAyNTYzMjk5MzAyNjUyMzI2Ng.G1pHyh.8lYh65h9k8cCRlPybTQK6ZWG6ECNX5H_p0-Aho"
 }
 keep_alive()
 file = open("text.txt", "r")
