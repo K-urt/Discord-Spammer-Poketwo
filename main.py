9c546beeeed270a55b7941ee9d25901ebad6a0cb
K-urt <131893438+K-urt@users.noreply.github.com>
2024-10-11 12:10:43 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 channelID = 1100680494766620758
 headers = {
     "authorization":
-    "MTAyNTYzMjk5MzAyNjUyMzI2Ng.G1pHyh.8lYh65h9k8cCRlPybTQK6ZWG6ECNX5H_p0-Aho"
+    ""
 }
 keep_alive()
 file = open("text.txt", "r")
